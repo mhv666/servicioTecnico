0c0fbc1f4c493c753880ce37850857f62b8c2a5d
Marti H <marti_horrach#@hotmail.com>
2019-01-31 00:42:53 +0100

    Añadido el combobox de marcas

--- Web de pruebas/index.js
+++ Web de pruebas/index.js
@@ -2,6 +2,8 @@
   websocket = new WebSocket(wsUri);
   var output;
   var recivedMessage;
+  var parser;
+  var xmlDoc;
   function init()
   {
     error = document.getElementById("error");
@@ -40,8 +42,26 @@
   function onMessage(evt)
   {
     recivedMessage = evt.data;
+
     console.log(evt.data);
-    
+    parser = new DOMParser();
+    xmlDoc = parser.parseFromString(recivedMessage,"text/xml");
+
+    if (xmlDoc.getElementsByTagName("tipo")[0].childNodes[0].nodeValue = "modelos_result") {
+      for (let i = 0; i < xmlDoc.getElementsByTagName("dispositivo").length; i++) {
+       // var marca = xmlDoc.getElementsByTagName("dispositivo")[i].childNodes[0].childNodes[0].nodeValue;
+        var marca = xmlDoc.getElementsByTagName("marca")[i].childNodes[0].nodeValue;
+        var newOption = document.createElement("option")
+        newOption.value = marca;
+        newOption.innerHTML = marca ;
+        
+           document.getElementById("marca").append(newOption)     
+      // document.getElementById("marca").childNodes[0].innerHTML("<option>"+ +"</option>");
+      }
+    }
+
+
+
     //writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data+'</span>');
     websocket.close();
   }
@@ -62,5 +82,21 @@
       error.innerHTML = message;
     
   }
+   function readContentOfTag(xml, nombreTag)
+   {
+     
 
+   }
+  /*
+  QString XmlHandler::readContentOfTag( QString xml ,QString nombreTag)
+{
+
+QDomDocument doc;
+doc.setContent(xml);
+QDomNodeList list = doc.elementsByTagName(nombreTag);
+QString model= list.at(0).toElement().text();
+return model;
+}
+
+  */
   window.addEventListener("load", init, false);
